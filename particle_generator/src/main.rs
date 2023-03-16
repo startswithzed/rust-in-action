397f1cd42b9d00d633305fc48070c4c7ba0e663f
Zaid <iamzaidsheikh@gmail.com>
2023-03-16 23:57:58 +0530

    feat(particle-generator): add runner code for app

--- particle_generator/src/main.rs
+++ particle_generator/src/main.rs
@@ -151,5 +151,27 @@ impl World {
 }
 
 fn main() {
+    let (width, height) = (1280.0, 960.0);
+    let mut window: PistonWindow = WindowSettings::new(
+        "particles", [width, height]
+    )
+        .exit_on_esc(true)
+        .build()
+        .expect("could not create a window.");
 
+    let mut world = World::new(width, height);
+    world.add_shapes(1000);
+
+    while let Some(event) = window.next() {
+        world.update();
+
+        window.draw_2d(&event, |ctx, renderer, _device| {
+            clear([0.15, 0.17, 0.17, 0.9], renderer);
+
+            for s in &mut world.particles {
+                let size = [s.position[0], s.position[1], s.width, s.height];
+                rectangle(s.color, size, ctx.transform, renderer);
+            }
+        });
+    }
 }
