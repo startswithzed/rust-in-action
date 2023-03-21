ebbd874aa3fc1dd04a15b8ce33e04774c8449b0d
Zaid <iamzaidsheikh@gmail.com>
2023-03-21 20:50:37 +0530

    feat(actionkv): add impl for open library function

--- actionkv/src/lib.rs
+++ actionkv/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{collections::HashMap, fs::OpenOptions};
+use std::fs::File;
+use serde_derive::{Deserialize, Serialize};
+use std::path::Path;
+use std::io::{self, BufReader, SeekFrom};
+
+type ByteString = Vec<u8>; // String in the form of raw bytes
+
+type ByteStr = [u8]; // str in the form of raw bytes
+
+#[derive(Debug, Serialize, Deserialize)] // generate serialized code to write k, v pairs to disk
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub struct ActionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64> // mapping b/w keys and file locations
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ActionKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .append(true)
+                        .open(path)?;
+        let index = HashMap::new();
+
+        Ok(ActionKV { f, index })
     }
 }
