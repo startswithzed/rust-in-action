c8b4d520b6530b918e988887c70deb0004d7f4a3
Zaid <iamzaidsheikh@gmail.com>
2023-04-02 14:25:02 +0530

    feat(clock): add os error handling

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -130,7 +130,17 @@ fn main() {
         let err_msg = format!("Unable to parse {} according to {}", t_, std);
         let t = parser(t_).expect(&err_msg);
 
-        Clock::set(t)
+        Clock::set(t);
+
+        let maybe_error = std::io::Error::last_os_error();
+        let os_error_code = &maybe_error.raw_os_error();
+
+        // matching on integers instead of enum, skips type safety and not fit for production
+        match os_error_code {
+            Some(0) => (),
+            Some(_) => eprintln!("Unable to set the time: {:?}", maybe_error),
+            None => (),
+        }
     }
 
     let now = Clock::get();
