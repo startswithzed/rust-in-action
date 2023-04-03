5efb132235b895453ab41deb3e0cb607dfe4af68
zaid <iamzaidsheikh@gmail.com>
2023-04-03 11:42:17 +0530

    feat(render-hex): init project with dependency

---
+++ render-hex/src/main.rs
@@ -0,0 +1,6 @@
+/// Render Hex generates unique pictoral avatars for users
+/// By taking the usernames and the digest of a hash function
+/// and generating parallax lines using the above inputs.
+/// Any stream of valid Base 16 bytes will generate a unique image in SVG format.
+
+fn main() {}
